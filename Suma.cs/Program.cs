283b6c96cae2bf4a47938d86a5707fa2325b18e3
kevin <kevin@LEO>
2025-08-26 23:55:07 -0500

    Agregar archivos de proyecto.

---
+++ Suma.cs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Suma.cs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, Suma;
+            Console.WriteLine("Ingrese 2 numeros: ");
+            n1 = int.Parse(Console.ReadLine());
+            n2 = int.Parse(Console.ReadLine());
+
+            Suma = n1 + n2;
+            Console.WriteLine("El resultado de la multiplicacion es: " + Suma);
+        }
+    }
+}
